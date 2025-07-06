7eaef2729d604a1636dfd6014d060fc8dae5c6ca
schuggermi <mschuchowski@gmx.de>
2025-07-07 00:20:13 +0200

    show content in page, adds github action workflow

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import P from './pokemon-api.js';
 
 // Test the connection
 P.getPokemonByName("eevee")
-    .then(function(response) {
+    .then(function (response) {
         console.log('Pokemon data:', response);
     })
     .catch(error => console.error('API Error:', error));
@@ -12,9 +12,24 @@ async function getPokemon() {
     try {
         const golduck = await P.getPokemonByName("golduck");
         console.log(golduck);
+        return golduck
     } catch (error) {
         console.error('Error fetching Pokemon:', error);
     }
 }
 
-getPokemon();
+// Wait for DOM to load and then update the content
+document.addEventListener('DOMContentLoaded', async () => {
+    const pokemon = await getPokemon();
+    console.log('Pokemon: ');
+    console.log(pokemon);
+    const pokemonsContainer = document.getElementById('pokemons');
+    pokemonsContainer.textContent = JSON.stringify(pokemon, null, 2);
+});
+
+// Keep the test connection if you want
+P.getPokemonByName("eevee")
+    .then(function (response) {
+        console.log('Pokemon data:', response);
+    })
+    .catch(error => console.error('API Error:', error));
