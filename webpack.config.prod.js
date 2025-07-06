ae3c2f9297e2704a0b812a182ec7e465cdec6adf
schuggermi <mschuchowski@gmx.de>
2025-07-07 00:23:26 +0200

    fixing deploy errors

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,22 +1,22 @@
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js', to: 'js' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
-      ],
-    }),
-  ],
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/img', to: 'img', noErrorOnMissing: true},
+                {from: 'src/css', to: 'css', noErrorOnMissing: true},
+                {from: 'src/js', to: 'js', noErrorOnMissing: true},
+                {from: 'src/robots.txt', to: 'robots.txt', noErrorOnMissing: true},
+                {from: 'src/site.webmanifest', to: 'site.webmanifest', noErrorOnMissing: true},
+            ],
+        }),
+    ],
 });
